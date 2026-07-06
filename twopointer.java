8fe39e2ae43a45a372d53ae2f51d3ade81e05488
Preetha D <preethapri2006@gmail.com>
2026-07-06 14:54:47 +0530

    p

---
+++ twopointer.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={13,2,4,15,12,10,6,5};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		int left=0;
+		int right=arr.length-1;
+		while(left<right){
+		 int temp=arr[left];
+		 arr[left]=arr[right];
+		 System.out.print(arr[left]+" ");
+		 arr[right]=temp;
+		 System.out.print(arr[right]+" ");
+		 left++;
+		 right--;
+		}
+	
+	}
+}
